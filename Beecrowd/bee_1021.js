ac0b12be5588ba2d6a7250fe933ace16db8ed873
Elize Piovesan <elizepiovesan.sci@gmail.com>
2026-01-21 21:37:42 -0300

    ajuste no exercicio devido a erro na submissão envolvendo ponto flutuante

--- Beecrowd/bee_1021.js
+++ Beecrowd/bee_1021.js
@@ -17,109 +17,36 @@ Obs: Utilize ponto (.) para separar a parte decimal.
 
 let valor_original = 576.73 
 let valor = 0
-
-let qtde_n100 = 0
-let qtde_n50 = 0
-let qtde_n20 = 0
-let qtde_n10 = 0
-let qtde_n5 = 0
-let qtde_n2 = 0
+let i = 0
 
 
-let qtde_m1 = 0
-let qtde_m050 = 0
-let qtde_m025 = 0
-let qtde_m010 = 0
-let qtde_m005 = 0
-let qtde_m001 = 0
+let notas_moedas = [100, 50, 20, 10, 5, 2, 1.00, 0.50, 0.25, 0.10, 0.05, 0.01]
+let qtde_notas_moedas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let notas_moedas_aux = []
 
-valor = valor_original
-while (valor != 0){
-    // notas de 100
-    if (valor >= 100){
-        qtde_n100 = Math.floor(valor/100)
-        valor = Number((valor % 100).toFixed(2)) // toFixed(2) => arrendondar para 2 casas decimais   
+
+
+valor = Math.round(valor_original * 100)
+let n = notas_moedas.length
+
+for (i=0; i < n; i++){
+    notas_moedas_aux[i] = notas_moedas[i] * 100
+}
+
+i=0
+while (i < n){
+    if (valor >= notas_moedas_aux[i]){
+        qtde_notas_moedas[i] = Math.floor(valor/notas_moedas_aux[i])
+        valor = valor % notas_moedas_aux [i]
     }
-    // notas de 50
-    else if (valor >= 50){
-        qtde_n50 = Math.floor(valor/50)
-        valor = Number((valor % 50).toFixed(2))
-    }
-
-    // notas de 20
-    else if (valor >= 20){
-        qtde_n20 = Math.floor(valor/20)
-        valor = Number((valor % 20).toFixed(2))
-    }
-
-    // notas de 10
-    else if (valor >= 10){
-        qtde_n10 = Math.floor(valor/10)
-        valor = Number((valor % 10).toFixed(2))
-    }
-    
-    // notas de 5
-    else if (valor >= 5){
-        qtde_n5 = Math.floor(valor/5)
-        valor = Number((valor % 5).toFixed(2))
-    }
-
-    // notas de 2
-    else if (valor >= 2){
-        qtde_n2 = Math.floor(valor/2)
-        valor = Number((valor % 2).toFixed(2))
-    }
-
-    // moedas de 1
-    else if (valor >= 1.00){
-        qtde_m1 = Math.floor(valor/1)
-        valor = Number((valor % 1).toFixed(2))
-    }
-
-    // moedas de 0.50
-    else if (valor >= 0.50){
-        qtde_m050 = Math.floor(valor/0.50)
-        valor = Number((valor % 0.50).toFixed(2))
-    }
-
-    // moedas de 0.25
-    else if (valor >= 0.25){
-        qtde_m025 = Math.floor(valor/0.25)
-        valor = Number((valor % 0.25).toFixed(2))
-    }
-
-    // moedas de 0.10
-    else if (valor >= 0.10){
-        qtde_m010 = Math.floor(valor/0.10)
-        valor = Number((valor % 0.10).toFixed(2))
-    }
-
-    // moedas de 0.05
-    else if (valor >= 0.05){
-        qtde_m005 = Math.floor(valor/0.05)
-        valor = Number((valor % 0.05).toFixed(2))
-    }
-    
-    // moedas de 0.01
-    else if (valor >= 0.01){
-        qtde_m001 = Math.floor(valor/0.01)
-        valor = Number((valor % 0.01).toFixed(2))
-    }
-
+    i++
 }
 
 console.log("NOTAS:")
-console.log(qtde_n100 + " nota(s) de R$100.00")
-console.log(qtde_n50 + " nota(s) de R$50.00")
-console.log(qtde_n20 + " nota(s) de R$20.00")
-console.log(qtde_n10 + " nota(s) de R$10.00")
-console.log(qtde_n5 + " nota(s) de R$5.00")
-console.log(qtde_n2 + " nota(s) de R$2.00\n")
-
+for (i=0; i<6 ; i++){
+    console.log(qtde_notas_moedas[i] + " nota(s) de R$ " + notas_moedas[i].toFixed(2))
+} 
 console.log("MOEDAS:")
-console.log(qtde_m1 + " moeda(s) de R$1.00")
-console.log(qtde_m050 + " moeda(s) de R$0.50")
-console.log(qtde_m025 + " moeda(s) de R$0.25")
-console.log(qtde_m010 + " moeda(s) de R$0.10")
-console.log(qtde_m005 + " moeda(s) de R$0.05")
-console.log(qtde_m001 + " moeda(s) de R$0.01")
+for (i=6; i < qtde_notas_moedas.length; i++){
+    console.log(qtde_notas_moedas[i] + " moeda(s) de R$ " + notas_moedas[i].toFixed(2))
+} 
